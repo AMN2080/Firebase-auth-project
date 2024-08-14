546b5b117d796d1fe70a2442120ce844d4cfb0bd
Alireza Mohamadzadeh <95294495+AMN2080@users.noreply.github.com>
2024-08-14 17:12:12 +0200

    complete style of login form

--- src/components/templates/LoginForm/index.tsx
+++ src/components/templates/LoginForm/index.tsx
@@ -1,7 +1,34 @@
 export default function LoginForm() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <p className="text-red-700">mamad</p>
-    </div>
+    <form className="w-96 p-10 bg-slate-100 shadow-lg rounded-3xl flex flex-col gap-5">
+      <div className="flex flex-col">
+        <label htmlFor="email" className="ml-4">
+          Email
+        </label>
+        <input
+          id="email"
+          placeholder="Email address"
+          type="text"
+          className="w-full h-10 outline-none rounded-3xl border px-2 text-center"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="password" className="ml-4">
+          Password
+        </label>
+        <input
+          id="password"
+          placeholder="Password"
+          type="password"
+          className="w-full h-10 outline-none rounded-3xl border px-2 text-center"
+        />
+      </div>
+      <button
+        type="submit"
+        className="h-10 mt-5 py-2 px-4 transition-colors duration-300 bg-slate-400 hover:bg-slate-300 text-slate-700 font-bold rounded"
+      >
+        Submit
+      </button>
+    </form>
   );
 }
--- src/pages/AuthPage/index.tsx
+++ src/pages/AuthPage/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthPage() {
   return (
-    <div className="w-full h-screen bg-slate-600 flex justify-center items-center">
+    <div className="w-full h-screen bg-slate-500 flex justify-center items-center">
       <Outlet />
     </div>
   );
