702fcde2873a9eab58516a35c8ad6c58771055ba
Alireza Mohamadzadeh <95294495+AMN2080@users.noreply.github.com>
2024-08-14 17:19:53 +0200

    complete style of register form

--- src/components/templates/RegisterForm/index.tsx
+++ src/components/templates/RegisterForm/index.tsx
@@ -1,3 +1,54 @@
 export default function RegisterForm() {
-  return <></>;
+  return (
+    <form className="w-96 p-10 bg-slate-100 shadow-lg rounded-3xl flex flex-col gap-5">
+      <div className="flex flex-col">
+        <label htmlFor="email" className="ml-4">
+          Email
+        </label>
+        <input
+          id="email"
+          placeholder="Email address"
+          type="text"
+          className="w-full h-10 outline-none rounded-3xl border px-2 text-center"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="password" className="ml-4">
+          Password
+        </label>
+        <input
+          id="password"
+          placeholder="Password"
+          type="password"
+          className="w-full h-10 outline-none rounded-3xl border px-2 text-center"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="confirmPassword" className="ml-4">
+          Confirm Password
+        </label>
+        <input
+          id="confirmPassword"
+          placeholder="Confirm Password"
+          type="password"
+          className="w-full h-10 outline-none rounded-3xl border px-2 text-center"
+        />
+      </div>
+      <div className="flex gap-2">
+        <input id="rules" type="checkbox" />
+        <label htmlFor="rules">
+          I accept the{" "}
+          <a href="#" className="text-blue-500 border-blue-400 border-b-2">
+            conditions
+          </a>
+        </label>
+      </div>
+      <button
+        type="submit"
+        className="h-10 mt-5 py-2 px-4 transition-colors duration-300 bg-slate-400 hover:bg-slate-300 text-slate-700 font-bold rounded"
+      >
+        Submit
+      </button>
+    </form>
+  );
 }
