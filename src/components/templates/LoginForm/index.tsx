bad4faa34da1012c5e2f2792142ed549df318dc6
Alireza Mohamadzadeh <95294495+AMN2080@users.noreply.github.com>
2024-08-17 17:15:17 +0200

    loginForm complete

--- src/components/templates/LoginForm/index.tsx
+++ src/components/templates/LoginForm/index.tsx
@@ -23,48 +23,51 @@ export default function LoginForm() {
 
   return (
     <>
-      {userLoggedIn && <Navigate to="/" />}
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="w-96 p-10 bg-slate-100 shadow-lg rounded-3xl flex flex-col gap-5"
-      >
-        <div className="flex flex-col">
-          {errors.email && (
-            <p className="text-red-500">{errors.email.message}</p>
-          )}
-          <label htmlFor="email" className="ml-4">
-            Email
-          </label>
-          <input
-            id="email"
-            placeholder="Email address"
-            type="text"
-            {...register("email")}
-            className="w-full h-10 outline-none rounded-3xl border px-2 text-center"
-          />
-        </div>
-        <div className="flex flex-col">
-          {errors.password && (
-            <p className="text-red-500">{errors.password.message}</p>
-          )}
-          <label htmlFor="password" className="ml-4">
-            Password
-          </label>
-          <input
-            id="password"
-            placeholder="Password"
-            type="password"
-            {...register("password")}
-            className="w-full h-10 outline-none rounded-3xl border px-2 text-center"
-          />
-        </div>
-        <button
-          type="submit"
-          className="h-10 mt-5 py-2 px-4 transition-colors duration-300 bg-slate-400 hover:bg-slate-300 text-slate-700 font-bold rounded"
+      {userLoggedIn ? (
+        <Navigate to="/" />
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-96 p-10 bg-slate-100 shadow-lg rounded-3xl flex flex-col gap-5"
         >
-          Submit
-        </button>
-      </form>
+          <div className="flex flex-col">
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+            <label htmlFor="email" className="ml-4">
+              Email
+            </label>
+            <input
+              id="email"
+              placeholder="Email address"
+              type="text"
+              {...register("email")}
+              className="w-full h-10 outline-none rounded-3xl border px-2 text-center"
+            />
+          </div>
+          <div className="flex flex-col">
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+            <label htmlFor="password" className="ml-4">
+              Password
+            </label>
+            <input
+              id="password"
+              placeholder="Password"
+              type="password"
+              {...register("password")}
+              className="w-full h-10 outline-none rounded-3xl border px-2 text-center"
+            />
+          </div>
+          <button
+            type="submit"
+            className="h-10 mt-5 py-2 px-4 transition-colors duration-300 bg-slate-400 hover:bg-slate-300 text-slate-700 font-bold rounded"
+          >
+            Submit
+          </button>
+        </form>
+      )}
     </>
   );
 }
