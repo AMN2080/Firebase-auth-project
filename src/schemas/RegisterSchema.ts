cb5a5a542d050ba4227c04d38691c94a8b4331df
Alireza Mohamadzadeh <95294495+AMN2080@users.noreply.github.com>
2024-08-17 17:53:08 +0200

    fix RegisterSchema

--- src/schemas/RegisterSchema.ts
+++ src/schemas/RegisterSchema.ts
@@ -9,14 +9,8 @@ export const RegisterSchema = z
     password: z
       .string()
       .min(8, "Password must be at least 8 characters long")
-      .regex(
-        /[a-zA-Z]/,
-        "Password must contain at least one letter and one number",
-      )
-      .regex(
-        /[0-9]/,
-        "Password must contain at least one letter and one number",
-      ),
+      .regex(/[a-zA-Z]/, "Password must contain both letters and numbers")
+      .regex(/[0-9]/, "Password must contain both letters and numbers"),
     confirmPassword: z.string().min(1, "Please confirm your password"),
     rules: z.boolean().refine((v) => v === true, {
       message: "Please agree to the terms and conditions",
