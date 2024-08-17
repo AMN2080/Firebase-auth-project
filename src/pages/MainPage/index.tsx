a43a9ec8d2edfc91f9e1634fd165e5bc5bffa410
Alireza Mohamadzadeh <95294495+AMN2080@users.noreply.github.com>
2024-08-17 17:38:39 +0200

    fix sign out

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,3 +1,24 @@
+import { doSignOut } from "../../firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 export default function MainPage() {
-  return <p className="text-yellow-500">welcome</p>;
+  const navigate = useNavigate();
+
+  const signOutHandler = async () => {
+    const result = await doSignOut();
+    console.log(result);
+    navigate("/auth/login");
+  };
+
+  return (
+    <div>
+      <p>welcome</p>
+      <button
+        className="h-10 mt-5 py-2 px-4 transition-colors duration-300 bg-slate-400 hover:bg-slate-300 text-slate-700 font-bold rounded"
+        onClick={signOutHandler}
+      >
+        Sign out
+      </button>
+    </div>
+  );
 }
