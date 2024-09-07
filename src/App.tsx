13956aa9cf0d0034dd78707032af1dfff8694845
Alireza Mohamadzadeh <95294495+AMN2080@users.noreply.github.com>
2024-09-07 16:13:39 +0330

    delete AppRouter and use router directly

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, useRoutes } from "react-router-dom";
 import routes from "./routes";
 
-function AppRoutes() {
-  return useRoutes(routes);
-}
-
 export default function App() {
+  const router = useRoutes(routes);
+  
   return (
     <BrowserRouter>
-      <AppRoutes />
+      {router}
     </BrowserRouter>
   );
 }
